9f391e4f2d4ee470e2d1188b67d92e4c479bd8c6
Mohdzahid0786 <mohmmadzahid1999@gmail.com>
2022-08-24 20:26:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Component/Navbar';
+import Service from './Component/Service';
+import About from './Component/About';
+import Reservation from './Component/Reservation';
+import Team from './Component/Team';
+import Menu from './Component/Menu';
+import Testimonial from './Component/Testimonial';
+import Footer from './Component/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Service />
+      <About />
+      <Menu/>
+      <Reservation/>
+      <Team/>
+      <Testimonial/>
+      {/* <Footer/> */}
     </div>
   );
 }
